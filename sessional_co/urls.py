2c31286bf3df232ae6823c3f7dff0530f2110300
ShailRT <shailesh18h@gmail.com>
2024-03-05 12:29:17 +0530

    mar5

--- sessional_co/urls.py
+++ sessional_co/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.sessional_list, name="sessional-list"),
     path('<str:pk>/<str:field>', views.sessional_table_edit, name="sessional-table-edit"),
+    path('sessional-table-download/<str:pk>/', views.sessional_table_download, name="sessional-table-download"),
 ]
--- sessional_co/views.py
+++ sessional_co/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from django.contrib import messages
 from core.models import Subject, CourseOutcome
 from .models import SessionalTable
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Spacer, Paragraph
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.enums import TA_CENTER
+import pandas as pd
+import json
+from datetime import datetime 
 
 def sessional_list(request):
     if request.method == "POST":
@@ -80,4 +88,114 @@ def sessional_table_edit(request, pk, field):
         return redirect('sessional-table-edit', pk=pk, field=field)
     
     return render(request, 'sessional-table-edit.html', context)
+
+def sessional_table_download(request, pk):
+    sessional = SessionalTable.objects.filter(uuid=pk).first()
+    cos = CourseOutcome.objects.filter(subject=sessional.subject)
+    data = {
+        'Student': sessional.student_info
+    }
+
+    for co in cos:
+        data[f'CO{co.number}'] = {'CT1':{},
+                                  'CT2':{},
+                                  'PUT':{},
+                                  'A/T':{},}
+
+    for key, value in sessional.ct1.items():
+        for k, v in value.items():
+            # add validation
+            if v =='':
+                data[key]['CT1'][k] = 0
+            else:
+                data[key]['CT1'][k] = v
+    
+    for key, value in sessional.ct2.items():
+        for k, v in value.items():
+            # add validation
+            if v =='':
+                data[key]['CT2'][k] = 0
+            else:
+                data[key]['CT2'][k] = v
+        
+    for key, value in sessional.put.items():
+        for k, v in value.items():
+            # add validation
+            if v =='':
+                data[key]['PUT'][k] = 0
+            else:
+                data[key]['PUT'][k] = v
+
+    for key, value in sessional.assignment_tutorial.items():
+        for k, v in value.items():
+            # add validation
+            if v =='':
+                data[key]['A/T'][k] = 0
+            else:
+                data[key]['A/T'][k] = v
+    
+            
+    print(data)
+    df = pd.DataFrame(data)    
+
+    
+
+    
+
+    # df = pd.DataFrame(data)
+
+    current_datetime = datetime.now().strftime('%d_%H-%M-%S')
+
+    pdf_file_name = f'table-{sessional.subject.code}-{current_datetime}.pdf'
+    pdf = SimpleDocTemplate(pdf_file_name, pagesize=letter)
+
+    table_data = [df.columns.to_list()] + df.values.tolist()
+
+    table = Table(table_data)
+
+    # style = TableStyle([
+    #     ('BACKGROUND', (0, 0), (-1, 0), colors.grey),  # Header background color
+    #     ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),  # Header text color
+    #     ('ALIGN', (0, 0), (-1, -1), 'CENTER'),  # Center align all cells
+    #     ('GRID', (0, 0), (-1, -1), 1, colors.black),  # Grid lines
+    #     ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),  # Header font bold
+    #     ('BOTTOMPADDING', (0, 0), (-1, 0), 12),  # Header bottom padding
+    #     ('FONTNAME', (0, 1), (-1, -1), 'Helvetica'),  # Body font
+    #     ('FONTSIZE', (0, 0), (-1, -1), 8),  # Font size
+    #     ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),  # Inner grid lines
+    #     ('BOX', (0, 0), (-1, -1), 0.25, colors.black),  # Outer border
+    # ])
+    # table.setStyle(style)
+
+    # Add a centered heading above the table
+    heading = "Sessional Table"
+    heading_style = ParagraphStyle(
+        'Heading1',
+        parent=getSampleStyleSheet()['Heading1'],
+        alignment=TA_CENTER,
+    )
+    heading_paragraph = Paragraph(heading, heading_style)
+    elements = [heading_paragraph, Spacer(1, 12), table]
+
+    # Build the PDF document
+    pdf.build(elements)
+
+
+    with open(pdf_file_name, 'rb') as pdf_file:
+        response = HttpResponse(pdf_file.read(), content_type='application/pdf')
+        response['Content-Disposition'] = f'attachment; filename="{pdf_file_name}"'
+        return response
+    
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = f'attachment; filename="{pdf_file_name}.csv"'
+
+    # csv_writer = csv.writer(response)
+
+    # csv_writer.writerow(list(cos.first().program_outcome_priority.keys())+list(cos.first().program_specific_outcome_priority[selected_department].keys())+list(cos.first().program_educational_objective_priority[selected_department].keys()))
+    # for co in cos:
+    #     csv_writer.writerow(list(co.program_outcome_priority.values())+list(co.program_specific_outcome_priority[selected_department].values())+list(co.program_educational_objective_priority[selected_department].values()))
+    
+    # df.to_csv(response, index=False)
+    
+    # return response   
 
